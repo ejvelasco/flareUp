312a41e75312307f5b2135f7cb6807fc9581fa37
velascoDev <dracontium2@gmail.com>
2017-08-21 14:41:12 -0500

    ID3 algorithm improvements

--- examples/example.js
+++ examples/example.js
@@ -1,7 +1,7 @@
-const flareUp = require('../src/flareUp');
-// unprocessed data
-const data = [
-	['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'],
+const flareUp = require('../src/index');
+// data
+const attribs = ['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'];
+const examples = [
 	[true, false, false, true, 'Some', 3, false, true, 'French', '0-10', true],
 	[true, false, false, true, 'Full', 1, false, false, 'Thai', '30-60', false],
 	[false, true, false, false, 'Some', 1, false, false, 'Burger', '0-10', true],
@@ -15,15 +15,8 @@ const data = [
 	[false, false, false, false, 'None', 1, false, false, 'Thai', '0-10', false],
 	[true, true, true, true, 'Full', 1, false, false, 'Burger', '30-60', true]
 ];
-// prepare data
-const examples = flareUp.rows(data, 1, data.length);
-const example = examples[4];
-const N = examples.length;
-const M = examples[0].length;
-const attribs = flareUp.rows(data, 0, 1).slice(0, examples[0].length - 1);
-const labels = flareUp.columns(examples.slice(0, N), M - 1, M);
-// build model 
+// build model
 const classifier = new flareUp.classifiers.ID3Classifier();
-const tree = classifier.fit(attribs, examples, labels);
+const examplesProcessed = classifier.processData(attribs, examples, 'WillWait');
+const tree = classifier.fit(examplesProcessed);
 classifier.print();
-console.log(classifier.predict(tree, attribs, example));
