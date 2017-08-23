916e74b9bb6848d936899443f8566ac2fe380462
velascoDev <dracontium2@gmail.com>
2017-08-23 12:00:12 -0500

    improved classifier API

--- examples/example.js
+++ examples/example.js
@@ -10,29 +10,21 @@ function onEnd() {
 	// drop rows with missing values
 	const examples = dataSet.filter(row => !row.some(val => val === '?'));
 	// set index to split data
-	const i = Math.floor(examples.length * .80);
 	// since attribs are anonymous, assign them numerical names
 	const attribs = flareUp.range(examples[0].length);
 	// first column is the edible / poisonous label
 	const label = attribs[0];
 	// process data
 	const classifier = new flareUp.classifiers.ID3Classifier();
-	const examplesProcessed = flareUp.shuffle(classifier.processData(attribs, dataSet, label));
-	const trainSet = examplesProcessed.slice(0, i);
-	const testSet = examplesProcessed.slice(i, examplesProcessed.length);
+	const examplesProcessed = classifier.processData(attribs, dataSet, label);
+	const [trainSet, testSet] = classifier.split(examplesProcessed);
 	// construct tree from training set
 	const tree = classifier.train(trainSet);
 	// quick accuracy test with test set
-	let numRight = 0;
-	testSet.forEach((example) => {
-		const prediction = classifier.predict(tree, attribs, example);
-		if (prediction === example.label) {
-			numRight++;
-		}
-	});
-	console.log( 'Samples Correctly Predicted: '+ ((numRight / testSet.length) * 100).toFixed(2).toString() + '%');
+	const accuracy = classifier.accuracy(tree, attribs, testSet);
+	console.log( 'Percentage of Samples Correctly Predicted: ' + accuracy);
 }
-// parse csv data into array
+// // parse csv data into array
 const dataSet = [];
 const stream = fs.createReadStream('mushrooms.csv');
 const csvStream = csv().on('data', onData).on('end', onEnd);
