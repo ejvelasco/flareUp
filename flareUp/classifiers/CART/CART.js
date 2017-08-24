e7e387bebff8bdb6d32b3629cbd7cfc6bac53a80
velascoDev <dracontium2@gmail.com>
2017-08-24 10:36:34 -0500

    began implementing CART Classifier

---
+++ flareUp/classifiers/CART/CART.js
@@ -0,0 +1,11 @@
+function buildTree() {
+
+}
+
+function CART(examples) {
+	const tree = {};
+	const labels = examples.map((example) => example.label);
+	console.log(examples);
+}
+
+module.exports = CART;
